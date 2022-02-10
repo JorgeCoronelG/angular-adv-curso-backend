698f494bfed977ff900de4547c10b6b14607d4ce
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-02-10 11:59:45 -0600

    Fin sección 15

--- controllers/auth.js
+++ controllers/auth.js
@@ -91,9 +91,13 @@ const renewToken = async (req, res = response) => {
     // Generar el token JWT
     const token = await generarJWT(uid);
 
+    // Obtener el usuario por uid
+    const usuario = await Usuario.findById(uid);
+
     res.json({
         ok: true,
-        token
+        token,
+        usuario
     });
 };
 
